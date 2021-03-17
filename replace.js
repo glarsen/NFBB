39acd020a14b1866cdec3abc44e852efbc246ebc
Garrett Larsen <garrett.larsen@protonmail.com>
2021-03-17 01:30:23 -0600

    Handle static pages

--- replace.js
+++ replace.js
@@ -2,18 +2,29 @@ const sanitize = function(node) {
     node.innerHTML = node.innerHTML.replaceAll('NFT', 'Digital Beanie Baby')
 }
 
-const options = {childList: true, subtree: true}
+const scan = function (node, tag) {
+    elements = node.getElementsByTagName(tag)
+    for (let i = 0; i < elements.length; i++) {
+        sanitize(elements[i])
+    }
+}
 
+// Handle static pages
+window.onload = function() {
+    scan(document.getRootNode(), 'p')
+    scan(document.getRootNode(), 'span')
+}
+
+// Monitor dynamic pages
+const options = {childList: true, subtree: true}
 const callback = function(mutations, observer) {
     mutations.forEach( (mutation) => {
         switch(mutation.type) {
             case 'childList':
                 for(const node of mutation.addedNodes) {
                     if (node.nodeType === 1) {
-                        spans = node.getElementsByTagName('span')
-                        for (let i = 0; i < spans.length; i++) {
-                            sanitize(spans[i])
-                        }
+                        scan(node, 'p')
+                        scan(node, 'span')
                     }
                 }
                 break;
@@ -22,5 +33,4 @@ const callback = function(mutations, observer) {
 }
 
 const observer = new MutationObserver(callback);
-
 observer.observe(document.getRootNode(), options)
